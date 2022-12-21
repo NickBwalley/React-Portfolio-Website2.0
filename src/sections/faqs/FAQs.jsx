d32b3cdf9a4ac625d059fee0a07391380146e229
NickBwalley <nickbwalley@gmail.com>
2022-12-21 20:21:13 +0300

    created components

---
+++ src/sections/faqs/FAQs.jsx
@@ -0,0 +1,11 @@
+import "./faqs.css";
+
+function FAQs() {
+  return (
+    <section id="faqs">
+      <h2>FAQS</h2>
+    </section>
+  );
+}
+
+export default FAQs;
---
+++ src/sections/services/Services.jsx
@@ -0,0 +1,11 @@
+import "./services.css";
+
+function Services() {
+  return (
+    <services id="services">
+      <h2>Services</h2>
+    </services>
+  );
+}
+
+export default Services;
